ec164f0419a1cfc28a7e417d8fc76ff053272d88
ezhuchenkov <37671925+ezhuchenkov@users.noreply.github.com>
2018-03-27 00:00:27 +0300

    SquareEquation + fibIter + fibRec +-

--- src/main/java/ru/alexander/learning/homework/Fibonacci.java
+++ src/main/java/ru/alexander/learning/homework/Fibonacci.java
@@ -3,20 +3,34 @@ package ru.alexander.learning.homework;
 public class Fibonacci {
 
     public static void main(String[] args) {
-
+        System.out.println("Метод итерации. Вывод числа, соответствующего задонному n:  "+fibIter(4));
+        System.out.println("Метод рекурсии. Вывод числа, соответствующего задонному n:  "+fibRec(4));
     }
-
-    //1 1 2 3 5 8 13
-
-    static int fibIten(int n) {
-        //todo
-
-        return 0;
+    static int fibIter(long n) {
+        int n1=1;
+        int n2=1;
+        int nx;
+        System.out.print("Метод интерации. Вывод чисел от 1 до n:"+"    "+n1+" "+n2+" ");
+        for (int i = 3; i <=n; i++) {
+            nx=n1+n2;
+            n1=n2;
+            n2=nx;
+            System.out.print(nx+" ");
+        }
+        System.out.println();
+        return n2;
     }
 
     static int fibRec(int n) {
-        //todo
-        return 0;
+        if (n==1 || n==2){
+        return 1;}
+        else {
+//            System.out.println("n="+n);
+//            int q=fibRec(n-1)+ fibRec(n-2);
+//            System.out.print("Пытаемся получить значения q="+q+" ");
+//            System.out.println();
+            return fibRec(n-1)+ fibRec(n-2);
+        }
     }
 
 }
--- src/main/java/ru/alexander/learning/homework/SquareEquation.java
+++ src/main/java/ru/alexander/learning/homework/SquareEquation.java
@@ -3,13 +3,73 @@ package ru.alexander.learning.homework;
 public class SquareEquation {
 
     public static void main(String[] args) {
-
+        solveAndPrint(2,5,2);
     }
 
     //a * x^2 + b * x + c = 0 //квадратное уравнение, линейное уравнение
     static void solveAndPrint(int a, int b, int c) {
-        //todo
-        //double koren = Math.sqrt(9); //3.0
-    }
+        if (a == 0 && b!=0) {
+            System.out.println("Простое уравнение:");
+            double x = (double)-c / b;
+            System.out.println("x=" + x);
+        }
+        else if (a == 0 && b==0) {
+            System.out.println("Нет решения, введено уравнение "+c+"=0");
+        }
+            else if (b == 0 && c!=0) {
+                System.out.println("Неполное квадратное уравнение:");
+                double xl = (double) Math.sqrt(c/a);
+                double x2 = (double) -Math.sqrt(c/a);
+                System.out.println("x1=" + xl);
+                System.out.println("x2=" + x2);
+                int p1= (int) ((a*Math.pow(xl,2))+(b*xl)+c);
+                int p2= (int) ((a*Math.pow(x2,2))+(b*x2)+c);
+            System.out.println("Проверка: "+a+"*"+xl+"^2+"+b+"*"+xl+c+"="+p1);
+            System.out.println("Проверка: "+a+"*"+x2+"^2+"+b+"*"+x2+c+"="+p2);
+            }
+        else if (b != 0 && c==0) {
+            System.out.println("Неполное квадратное уравнение:");
+            double xl = 0;
+            double x2 = (double)-b/a;
+            System.out.println("x1=" + xl);
+            System.out.println("x2=" + x2);
+            int p1= (int) ((a*Math.pow(xl,2))+(b*xl)+c);
+            int p2= (int) ((a*Math.pow(x2,2))+(b*x2)+c);
+            System.out.println("Проверка: "+a+"*"+xl+"^2+"+b+"*"+xl+"+"+c+"="+p1);
+            System.out.println("Проверка: "+a+"*"+x2+"^2+"+b+"*"+x2+"+"+c+"="+p2);
+        }
+        else if (b == 0 && c==0) {
+            System.out.println("Неполное квадратное уравнение:");
+            double xl = 0;
+            System.out.println("x=" + xl);
+            }
+         else {
+            System.out.println("Квадратное уравнение:");
+            double x1;
+            double x2;
+            double d = Math.pow(b, 2) - (4 * a * c); //почему ругается на int d?
+            double sqrt = Math.sqrt(d);
+            if (d > 0) {
+                x1 = (-b + sqrt) / (2 * a);
+                x2 = (-b - sqrt) / (2 * a);
+                System.out.println("Уравнение решено, дискриминант больше 0, варианты решений: ");
+                System.out.println("x1 = " + x1);
+                System.out.println("x2 = " + x2);
+                int p1= (int) ((a*Math.pow(x1,2))+(b*x1)+c);
+                int p2= (int) (a*Math.pow(x2,2)+b*x2+c);
+                System.out.println("Проверка: "+a+"*"+x1+"^2+"+b+"*"+x1+"+"+c+"="+p1);
+                System.out.println("Проверка: "+a+"*"+x2+"^2+"+b+"*"+x2+"+"+c+"="+p2);
 
+            } else if (d == 0) {
+                x1 = (-b + sqrt) / (2 * a);
+                System.out.println("Уравнение решено, дискриминант равен 0, решение: ");
+                System.out.println("x = " + x1);
+                int p1= (int) ((a*Math.pow(x1,2))+(b*x1)+c);
+                System.out.println("Проверка: "+a+"*"+x1+"^2+"+b+"*"+x1+"+"+c+"="+p1);
+
+            } else
+                System.out.println("Уравнение не имеет решения");
+
+        }
+    }
 }
