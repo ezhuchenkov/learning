884f730719162def67479224564380828508d3db
Ezhuchenkov <ezhuchenkov@gmail.com>
2018-04-16 22:25:38 +0300

    Оптимизировал SquareEquation, есть вопрос по памяти

--- src/main/java/ru/alexander/learning/homework/SquareEquation.java
+++ src/main/java/ru/alexander/learning/homework/SquareEquation.java
@@ -3,7 +3,7 @@ package ru.alexander.learning.homework;
 public class SquareEquation {
 
     public static void main(String[] args) {
-        solveAndPrint(13, 0, 2);
+        solveAndPrint(2, 6, 1);
 
         //b ^ false = b
         //b ^ b = false
@@ -23,8 +23,6 @@ public class SquareEquation {
     }
 
     static void solveAndPrint(int a, int b, int c) {
-
-
         if (a == 0) {
             if (b == 0) {
                 if (c == 0) {
@@ -37,55 +35,35 @@ public class SquareEquation {
                 double x = ((double) -c) / b;
                 System.out.println("x=" + x);
             }
-        } else if (b == 0) {
-            if (c == 0) {
-                System.out.println("x=0");
-            } else {
-                System.out.println("Incomplete square equation:");
-                /*a=13,b=0,c=2, q=0????*/
-//                double q=-c/a;
-//                System.out.println(q);
-                if (-c / a >= 0) {
-                    double xl = (double) Math.sqrt(-c / a);
-                    double x2 = (double) -Math.sqrt(-c / a);
-                    System.out.println("x1=" + xl);
-                    System.out.println("x2=" + x2);
-                    int p1 = (int) ((a * xl * xl) + (b * xl) + c);
+        }else{
+
+                System.out.println("Square Equation:");
+                double d = (b*b)- (4 * a * c);
+                double sqrt = Math.sqrt(d);
+                if (d > 0) {
+                    double x1 = (-b + sqrt) / (2 * a); //есть ли разница в памяти, если не создавать переменную, а вычислять ее 2 раза?
+                    double x2 = (-b - sqrt) / (2 * a);
+                    System.out.println("The equation is solved, the discriminant is greater than 0, the solutions are: ");
+                    System.out.println("x1 = " + x1);
+                    System.out.println("x2 = " + x2);
+                    int p1 = (int) ((a * x1 * x1) + (b * x1) + c);
                     int p2 = (int) ((a * x2 * x2) + (b * x2) + c);
-                    System.out.println("Checking x1: " + a + " * " + xl + "^2 + " + b + " * " + xl + " + " + c + "=" + p1);
-                    System.out.println("Checking x2: " + a + " * " + x2 + "^2 + " + b + " * " + x2 + " + " + c + "=" + p2);
-                } else {
-                    System.out.println("Equation has no solution");
-                }
+                    System.out.println("Checking x1: " + a + "*" + x1 + "^2+" + b + "*" + x1 + "+" + c + "=" + p1);
+                    System.out.println("Checking x2: " + a + "*" + x2 + "^2+" + b + "*" + x2 + "+" + c + "=" + p2);
+
+                } else if (d == 0) {
+                    double x1 = (-b + sqrt) / (2 * a);
+                    System.out.println("The equation is solved, the discriminant is equal to 0, the solution is: ");
+                    System.out.println("x = " + x1);
+                    int p1 = (int) ((a * x1 * x1) + (b * x1) + c);
+                    System.out.println("Checking: " + a + "*" + x1 + "^2+" + b + "*" + x1 + "+" + c + "=" + p1);
+
+                } else
+                    System.out.println("Equation has no solutions");
+
             }
-        } else {
-            System.out.println("Square Equation:");
-            double d = Math.pow(b, 2) - (4 * a * c);
-            double sqrt = Math.sqrt(d);
-            if (d > 0) {
-                double x1 = (-b + sqrt) / (2 * a);
-                double x2 = (-b - sqrt) / (2 * a);
-                System.out.println("The equation is solved, the discriminant is greater than 0, the solutions are: ");
-                System.out.println("x1 = " + x1);
-                System.out.println("x2 = " + x2);
-                int p1 = (int) ((a * x1 * x1) + (b * x1) + c);
-                int p2 = (int) ((a * x2 * x2) + (b * x2) + c);
-                System.out.println("Checking x1: " + a + "*" + x1 + "^2+" + b + "*" + x1 + "+" + c + "=" + p1);
-                System.out.println("Checking x2: " + a + "*" + x2 + "^2+" + b + "*" + x2 + "+" + c + "=" + p2);
-
-            } else if (d == 0) {
-                double x1 = (-b + sqrt) / (2 * a);
-                System.out.println("The equation is solved, the discriminant is equal to 0, the solution is: ");
-                System.out.println("x = " + x1);
-                int p1 = (int) ((a * x1 * x1) + (b * x1) + c);
-                System.out.println("Checking: " + a + "*" + x1 + "^2+" + b + "*" + x1 + "+" + c + "=" + p1);
-
-            } else
-                System.out.println("Equation has no solutions");
-
         }
     }
-}
 
 
 //            if (a == 0 && b != 0) {
