22598272dd3fa302e771ce0fba205705180b14b1
ezhuchenkov <37671925+ezhuchenkov@users.noreply.github.com>
2018-04-27 22:56:00 +0300

    метод "в лоб"

--- src/main/java/ru/alexander/learning/homework/RoboBattle.java
+++ src/main/java/ru/alexander/learning/homework/RoboBattle.java
@@ -11,20 +11,17 @@ public class RoboBattle {
     }
 
     static void recursion(int x1, int x3) {
-        if (x3==x4 && (x3 - x1) < 15){
+        if (x3 == x4 && (x3 - x1) < 15) {
             System.out.println("You lose");
-        }else{
-        if ((x3 - x1) > 15) {
-            x1 = x2;
-            x3 = x4;
-            recursion(x1, x3);
         } else {
-            System.out.println("You win!");
-            System.out.println("Роботы уничтожены в промежутке между " + x1 + " и " + x3);
-
+            if ((x3 - x1) > 15) {
+                x1 = x2;
+                x3 = x4;
+                recursion(x1, x3);
+            } else {
+                System.out.println("You win!");
+                System.out.println("Роботы уничтожены в промежутке между " + x1 + " и " + x3);
+            }
         }
-
-
-    }}
-
+    }
 }
