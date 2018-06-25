1a2295bc732aad6c8b4575ac8743fafe0de79c3d
ezhuchenkov <37671925+ezhuchenkov@users.noreply.github.com>
2018-06-25 23:03:04 +0300

    TelegramBot 25/06/18

--- src/main/java/ru/alexander/learning/telegram/ExchangeBot.java
+++ src/main/java/ru/alexander/learning/telegram/ExchangeBot.java
@@ -1,14 +1,24 @@
 package ru.alexander.learning.telegram;
 
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class ExchangeBot extends TelegramLongPollingBot {
-
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            if (message.getText().equals("/help"))
+                sendMsg(message, "Привет");
+            else
+                sendMsg(message, "напиши /help");
+        }
 
-    }
+        }
 
     @Override
     public String getBotUsername() {
@@ -20,4 +30,19 @@ public class ExchangeBot extends TelegramLongPollingBot {
         return "591243527:AAEA8b0xY42EvM7iKsMbM-v-mYBLo6x2AMI";
     }
 
+    protected ExchangeBot(DefaultBotOptions options) {
+        super(options);
+    }
+    private void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ru/alexander/learning/telegram/TelegramExample.java
+++ src/main/java/ru/alexander/learning/telegram/TelegramExample.java
@@ -1,15 +1,33 @@
 package ru.alexander.learning.telegram;
 
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.telegram.telegrambots.ApiContext;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 public class TelegramExample {
+    private static String PROXY_HOST = "vamtlgrm.drproxy.pro" /* proxy host */;
+    private static Integer PROXY_PORT = 1011 /* proxy port */;
+    static Update update = new Update();
 
     public static void main(String[] args) throws TelegramApiRequestException {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        telegramBotsApi.registerBot(new ExchangeBot());
+        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+        HttpHost httpHost = new HttpHost(PROXY_HOST, PROXY_PORT);
+        RequestConfig requestConfig = RequestConfig.custom().setProxy(httpHost).setAuthenticationEnabled(false).build();
+        botOptions.setRequestConfig(requestConfig);
+        botOptions.setHttpProxy(httpHost);
+
+        ExchangeBot bot = new ExchangeBot(botOptions);
+        telegramBotsApi.registerBot(bot);
+        bot.onUpdateReceived(update);
+
+
     }
 
 }
