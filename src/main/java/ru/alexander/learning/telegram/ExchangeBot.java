ebaae051398d63deb04a76c08329706f508c3950
ezhuchenkov <37671925+ezhuchenkov@users.noreply.github.com>
2018-06-27 00:30:41 +0300

    TelegramBot 25/06/18

--- src/main/java/ru/alexander/learning/telegram/ExchangeBot.java
+++ src/main/java/ru/alexander/learning/telegram/ExchangeBot.java
@@ -7,18 +7,62 @@ import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ExchangeBot extends TelegramLongPollingBot {
+
     @Override
     public void onUpdateReceived(Update update) {
-        Message message = update.getMessage();
-        if (message != null && message.hasText()) {
-            if (message.getText().equals("/help"))
-                sendMsg(message, "Привет");
-            else
-                sendMsg(message, "напиши /help");
-        }
 
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String user_first_name = update.getMessage().getChat().getFirstName();
+            String user_last_name = update.getMessage().getChat().getLastName();
+            String user_username = update.getMessage().getChat().getUserName();
+            long user_id = update.getMessage().getChat().getId();
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            String answer = message_text;
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(answer);
+            log(user_first_name, user_last_name, Long.toString(user_id), message_text, answer);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+//    @Override
+//    public void onUpdateReceived(Update update) {
+//        Message message = update.getMessage();
+//        if (message != null && message.hasText()) {
+//            if (message.getText().equals("/help"))
+//                sendMsg(message, "Привет");
+//            else
+//                sendMsg(message, "напиши /help");
+//        }
+
+//        SendMessage msg= new SendMessage().setText("hi");
+//        try { execute(msg);}
+//        catch (TelegramApiException e1){e1.printStackTrace();}
+//        // We check if the update has a message and the message has text
+//        if (update.hasMessage() && update.getMessage().hasText()) {
+//            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+//                    .setChatId(update.getMessage().getChatId())
+//                    .setText(update.getMessage().getText());
+//            try {
+//                execute(message); // Call method to send the message
+//            } catch (TelegramApiException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        }
 
     @Override
     public String getBotUsername() {
@@ -33,6 +77,7 @@ public class ExchangeBot extends TelegramLongPollingBot {
     protected ExchangeBot(DefaultBotOptions options) {
         super(options);
     }
+
     private void sendMsg(Message message, String text) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
@@ -44,5 +89,13 @@ public class ExchangeBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+    private void log(String first_name, String last_name, String user_id, String txt, String bot_answer) {
+        System.out.println("\n ----------------------------");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        System.out.println("Message from " + first_name + " " + last_name + ". (id = " + user_id + ") \n Text - " + txt);
+        System.out.println("Bot answer: \n Text - " + bot_answer);
     }
 }
--- src/main/java/ru/alexander/learning/telegram/TelegramExample.java
+++ src/main/java/ru/alexander/learning/telegram/TelegramExample.java
@@ -1,31 +1,51 @@
 package ru.alexander.learning.telegram;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.telegram.telegrambots.ApiContext;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class TelegramExample {
-    private static String PROXY_HOST = "vamtlgrm.drproxy.pro" /* proxy host */;
-    private static Integer PROXY_PORT = 1011 /* proxy port */;
-    static Update update = new Update();
+    private static String PROXY_HOST = "phobos.public.opennetwork.cc" /* proxy host */;
+    private static Integer PROXY_PORT = 1090 /* proxy port */;
+    private static String PROXY_USER = "201154259";
+    private static String PROXY_USER_PASS = "ytpgQagQ" /* proxy password */;
+    static CredentialsProvider credsProvider;
 
-    public static void main(String[] args) throws TelegramApiRequestException {
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
-        HttpHost httpHost = new HttpHost(PROXY_HOST, PROXY_PORT);
-        RequestConfig requestConfig = RequestConfig.custom().setProxy(httpHost).setAuthenticationEnabled(false).build();
-        botOptions.setRequestConfig(requestConfig);
-        botOptions.setHttpProxy(httpHost);
+    public static void main(String[] args) {
+        try {
 
-        ExchangeBot bot = new ExchangeBot(botOptions);
-        telegramBotsApi.registerBot(bot);
-        bot.onUpdateReceived(update);
+            ApiContextInitializer.init();
+            TelegramBotsApi botsApi = new TelegramBotsApi();
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+            credsProvider = new BasicCredentialsProvider();
+            credsProvider.setCredentials(
+                    new AuthScope(PROXY_HOST, PROXY_PORT),
+                    new UsernamePasswordCredentials(PROXY_USER, PROXY_USER_PASS));
+
+            HttpHost httpHost = new HttpHost(PROXY_HOST, PROXY_PORT);
+
+            RequestConfig requestConfig = RequestConfig.custom().setProxy(httpHost).setAuthenticationEnabled(true).build();
+
+            botOptions.setRequestConfig(requestConfig);
+            botOptions.setCredentialsProvider(credsProvider);
+            botOptions.setHttpProxy(httpHost);
+
+
+
+            botsApi.registerBot(new ExchangeBot(botOptions));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+
 
 
     }
