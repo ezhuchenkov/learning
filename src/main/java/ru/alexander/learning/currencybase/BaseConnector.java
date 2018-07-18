548fc77c948ac6c52aa5cc7f669a32e12cfd6516
ezhuchenkov <37671925+ezhuchenkov@users.noreply.github.com>
2018-07-19 00:13:07 +0300

    18/07/2018

--- src/main/java/ru/alexander/learning/currencybase/BaseConnector.java
+++ src/main/java/ru/alexander/learning/currencybase/BaseConnector.java
@@ -8,24 +8,24 @@ import java.util.Map;
 
 public class BaseConnector {
     private static final String URL_PAHT = "jdbc:postgresql://localhost:5432/postgres";
-    String USER = "postgres";
-    String PASSWORD = "1899181010";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "1899181010";
+    private static final String SQL = "INSERT INTO currency(valute,currency,date) VALUES (?,?,CURRENT_DATE)";
 
 
     protected void baseConnector() {
         try (Connection connection = DriverManager.getConnection(URL_PAHT, USER, PASSWORD)) {
             connection.setSchema("public");
             ValueLoader valueLoader = new ValueLoader();
-
-            for (Map.Entry<String, Valute> entry : valueLoader.getMoney().map.entrySet()) {
-                try (PreparedStatement ps = connection.prepareStatement(
-                        "INSERT INTO currency(valute,currency,date) VALUES (?,?,CURRENT_DATE)")) {
+            try (PreparedStatement ps = connection.prepareStatement(SQL)) {
+                for (Map.Entry<String, Valute> entry : valueLoader.getMoney().map.entrySet()) {
                     String name = entry.getKey();
                     double value = entry.getValue().value;
                     ps.setString(1, name);
                     ps.setDouble(2, value);
-                    ps.executeUpdate();
+                    ps.addBatch();
                 }
+                ps.executeBatch();
             }
         } catch (SQLException e) {
             throw new RuntimeException("Smth wrong with: " + URL_PAHT, e);
